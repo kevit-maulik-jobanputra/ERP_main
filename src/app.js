f520035f536ddfeffe907f0317bb887b2f1b409d
Maulik Jobanputra <kevit.maulik.jobanputra@gmail.com>
2023-03-21 15:15:09 +0530

    set-Up the server

---
+++ src/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const hpp = require('hpp');
+const helmet = require('helmet');
+const compression = require('compression');
+const cors = require('cors');
+
+class App{
+    constructor(){
+        this.app = express();
+        this.PORT = 3000;
+        this.router = express.Router();
+        this.router.get('/', (req,res) => {
+            res.send("Hello")
+        });
+        this.initializeRoutes();
+        this.initializeMiddleWares();
+    };
+
+    initializeRoutes(){
+        this.app.use('/', this.router);
+    };
+
+    initializeMiddleWares(){
+        this.app.use(hpp());
+        this.app.use(helmet());
+        this.app.use(compression());
+        this.app.use(cors());
+        this.app.use(bodyParser.urlencoded({extended:true}));
+        this.app.use(bodyParser.json());
+    };
+
+    listen(){
+        this.app.listen(this.PORT, () => {
+            console.log(`Server Up and running at ${this.PORT}!`)
+        });
+    };
+}
+
+module.exports = App;
