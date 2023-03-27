283efdea6a9be141bd946849dec6586ea6c4bd51
Maulik Jobanputra <kevit.maulik.jobanputra@gmail.com>
2023-03-27 13:33:14 +0530

    Added analytics query to get absentees on a specific day

--- src/components/attendances/attendances.DAL.js
+++ src/components/attendances/attendances.DAL.js
@@ -23,6 +23,102 @@ const findStudentById = async (id, next) => {
     } catch (error) {
         next(new Err(400, 'No such student found!', 'BAD_REQUEST'));
     }
+};
+
+const getAttendances = async (date, queryObj, next) => {
+    try {
+        return await Attendance.aggregate([[
+            {
+              $match:
+                {
+                  attendance: {
+                    $elemMatch: {
+                      date: new Date(date),
+                      isPresent: false,
+                    },
+                  },
+                },
+            },
+            {
+                $lookup: 
+                    {
+                    from: "students",
+                    localField: "student",
+                    foreignField: "_id",
+                    as: "student",
+                },
+            },
+            {
+                $project: {
+                    student: {
+                    $arrayElemAt: ["$student", 0],
+                    },
+                    _id: 0,
+                },
+            },
+            {
+                $lookup: {
+                    from: "batches",
+                    localField: "student.batch",
+                    foreignField: "_id",
+                    as: "student.batch",
+                },
+            },
+            {
+                $project: {
+                    firstName: "$student.firstName",
+                    lastName: "$student.lastName",
+                    contact: "$student.contact",
+                    currentSem: "$student.currentSem",
+                    branch: "$student.branch",
+                    batch: {
+                    $arrayElemAt: ["$student.batch", 0],
+                    },
+                },
+            },
+            {
+                $project: {
+                    firstName: 1,
+                    lastName: 1,
+                    contact: 1,
+                    currentSem: 1,
+                    batch: "$batch.year",
+                    branch: {
+                        $arrayElemAt: [
+                            {
+                            $filter: {
+                                input: "$batch.branches",
+                                as: "branches",
+                                cond: {
+                                $eq: [
+                                    "$branch",
+                                    "$$branches._id",
+                                ],
+                                },
+                            },
+                            },
+                            0,
+                        ],
+                    },
+                },
+            },
+            {
+                $project: {
+                    firstName: 1,
+                    lastName: 1,
+                    contact: 1,
+                    currentSem: 1,
+                    batch: 1,
+                    branch: "$branch.name",
+                },
+            },
+            {
+                $match: queryObj
+            }
+        ]])
+    } catch (error) {
+        next(new Err(500, 'An unexpected error occurred!', 'INTERNAL_SERVER_ERROR!', error));
+    }
 }
 
-module.exports = { addStudentToAttendance, removeStudentFromAttendance, findStudentById };
+module.exports = { addStudentToAttendance, removeStudentFromAttendance, findStudentById, getAttendances };
